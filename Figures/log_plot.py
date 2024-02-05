f70fa20fee5b858028b99300b4b45135dac6f184
PanoPepino <panocuenta@gmail.com>
2024-02-05 18:15:38 +0100

    Some progress to gathering a good collection

--- Figures/log_plot.py
+++ Figures/log_plot.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import unicodedata as ud
 
+
+
 def radiation(x):
     return 10/x**4
 
@@ -16,11 +18,15 @@ y_r = radiation(x)
 y_m = matter(x)
 y_d = dark_energy()
 y_d = np.ones(10000)
+my_colors = {"red" : "#9b0808",
+            "green" : "#006111",
+            "blue":"#0051CF",
+            "white": "#FFF5F0"}
 
 plt.figure()
-plt.plot(x, y_r, 'r.', markersize = 0.5, label = 'Radiation')
-plt.plot(x, y_m, 'g.', markersize = 0.5,label = 'Matter')
-plt.plot(x, y_d, 'b.', markersize = 0.5,label = 'Dark energy')
+plt.plot(x, y_r, color=my_colors['red'], marker='.',  markersize = 0.5, linewidth = 0,label = 'Radiation')
+plt.plot(x, y_m, color=my_colors['green'], marker='.', markersize = 0.5, linewidth = 0, label = 'Matter')
+plt.plot(x, y_d, color=my_colors['blue'], marker='.', markersize = 0.5, linewidth = 0, label = 'Dark energy')
 plt.axis
 
 #plt.xlabel('log (a)')
@@ -30,4 +36,4 @@ plt.yscale('log')
 plt.xscale('log')
 plt.xticks([])
 plt.yticks([])
-plt.savefig('Svgs/densities.png')
+plt.savefig('Svgs/densities.svg', dpi = 300)
